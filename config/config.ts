18243bef90ff02821e585733972205b6f9be476d
wjp <2435878928@qq.com>
2026-03-14 18:53:05 +0800

    modify config

--- config/config.ts
+++ config/config.ts
@@ -12,7 +12,7 @@ const { REACT_APP_ENV = "dev" } = process.env;
  * @description 部署时的路径，如果部署在非根目录下，需要配置这个变量
  * @doc https://umijs.org/docs/api/config#publicpath
  */
-const PUBLIC_PATH: string = "/user-center-front";
+const PUBLIC_PATH: string = "/user-center-front/";
 export default defineConfig({
   /**
    * @name 开启 hash 模式
